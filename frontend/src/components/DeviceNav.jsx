4067423afc215577f579c89051439923f9c58180
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-21 21:59:41 +0800

    IP Address btn txt color Fixed

--- frontend/src/components/DeviceNav.jsx
+++ frontend/src/components/DeviceNav.jsx
@@ -7,7 +7,7 @@ const DeviceNav = () => {
     <>
         <Nav variant='pills' defaultActiveKey='/devices' className='flex-column'>
             <Nav.Link className='bg-secondary my-1 py-3 shadow' href='/devices'><span className='h5'><strong><FaSun className='me-3' />All Devices</strong></span></Nav.Link>
-            <Nav.Link className='bg-secondary my-1 py-3 shadow' href='/ip'><span className='h5'><strong><FaSitemap className='me-3' />IP Address</strong></span></Nav.Link>
+            <Nav.Link className='bg-secondary text-light my-1 py-3 shadow' href='/ip'><span className='h5'><strong><FaSitemap className='me-3' />IP Address</strong></span></Nav.Link>
         </Nav>
     </>
   )
