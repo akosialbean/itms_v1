a2d7cff5a87bc975a378e51b2b9df70b4fec5d46
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-22 09:38:48 +0800

    IPAdd item brkpts Updated

--- frontend/pages/IpAddress.jsx
+++ frontend/pages/IpAddress.jsx
@@ -48,7 +48,7 @@ const IpAddress = () => {
                   {ip.length > 0 ? (
                     <Row>
                       {ip.map((item) => (
-                      <Col xs={12} md={4} lg={3} xl={2} className='my-1'>
+                      <Col xs={6} md={4} lg={3} xl={2} className='my-1'>
                         <Button variant={item.status === 'active' ? ('success') : ('danger')} className='shadow w-100'>
                           <FaNetworkWired className='me-5' /> {item.ip}
                         </Button>
