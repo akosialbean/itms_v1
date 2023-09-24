639b095059b655fb6dc6a5470bcef41570876c5a
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-24 10:38:14 +0800

    Dark Mode Config Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import TestHeader from "./Test/components/TestHeader";
+import TestLoginPage from "./Test/pages/TestLoginPage";
+import TestRegisterPage from "./Test/pages/TestRegisterPage";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import IconButton from "@mui/material/IconButton";
 
 const darkTheme = createTheme({
   palette: {
@@ -14,17 +19,31 @@ const darkTheme = createTheme({
   },
 });
 
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
+
 const App = () => {
+  const [themeMode, setThemeMode] = useState("dark");
+
+  const toggleThemeMode = () => {
+    setThemeMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  };
+
+  const theme = themeMode === "dark" ? darkTheme : lightTheme;
+
   return (
-    <>
-      <ThemeProvider theme={darkTheme}>
-        <ToastContainer />
-        <TestHeader />
-        <Container className="mt-5 pt-5">
-          <Outlet />
-        </Container>
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ToastContainer />
+        <TestHeader toggleThemeMode={toggleThemeMode} themeMode={themeMode} />
+      <Container className="mt-5 pt-5">
+        <TestLoginPage />
+        <TestRegisterPage />
+      </Container>
+    </ThemeProvider>
   );
 };
 
