0a69d35ea4af0f328376221a9d5f3e55adce7ffe
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-24 08:16:00 +0800

    Dark Mode by Default Applied

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,31 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import TestHeader from "./Test/components/TestHeader";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-import React from 'react'
-import { Container } from 'react-bootstrap'
-import { Outlet } from 'react-router-dom'
-import Header from './components/Header'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import TestHeader from './Test/components/TestHeader'
-import TestLoginPage from './Test/pages/TestLoginPage'
-import TestRegisterPage from './Test/pages/TestRegisterPage'
-
-
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App = () => {
   return (
     <>
-      {/* <Header />
-      <Container className='mt-5 pt-5'>
-        <Outlet />
-      </Container> */}
-      <ToastContainer />
-      <TestHeader/>
-      <TestLoginPage/>
-      <TestRegisterPage/>
+      <ThemeProvider theme={darkTheme}>
+        <ToastContainer />
+        <TestHeader />
+        <Container className="mt-5 pt-5">
+          <Outlet />
+        </Container>
+      </ThemeProvider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
