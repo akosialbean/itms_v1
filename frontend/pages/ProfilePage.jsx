a16eaaf4f47d839c905cbccd23324f547b093a51
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-25 12:58:07 +0800

    ProfilePage.jsx Implemented

--- frontend/pages/ProfilePage.jsx
+++ frontend/pages/ProfilePage.jsx
@@ -1,93 +1,213 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import FormContainer from '../src/components/FormContainer'
-import { useSelector, useDispatch } from 'react-redux'
-import { toast } from 'react-toastify'
-import Loader from '../src/components/Loader'
-import { setCredentials } from '../src/slices/authSlice'
-import { Form, Button } from 'react-bootstrap'
-import { useUpdateUserMutation } from '../src/slices/usersApiSlice'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  CircularProgress,
+  Card,
+  CardContent,
+  CardHeader,
+  InputAdornment,
+  IconButton,
+} from '@mui/material';
+import { useUpdateUserMutation } from '../src/slices/usersApiSlice';
+import { setCredentials } from '../src/slices/authSlice';
+import { toast } from 'react-toastify';
+import Loader from '../src/components/Loader';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const ProfilePage = () => {
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector((state) => state.auth);
+  const [updateProfile, { isLoading }] = useUpdateUserMutation();
 
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+  const [oldPassword, setOldPassword] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-    const { userInfo } = useSelector((state) => state.auth)
+  useEffect(() => {
+    setName(userInfo.name);
+    setEmail(userInfo.email);
+  }, [userInfo]);
 
-    const [updateProfile, {isLoading}] = useUpdateUserMutation()
+  const formik = useFormik({
+    initialValues: {
+      oldPassword: '',
+      name: '',
+      email: '',
+      newPassword: '',
+      confirmPassword: '',
+    },
+    validationSchema: Yup.object({
+      oldPassword: Yup.string().required('Old Password is required'),
+      name: Yup.string().required('Name is required'),
+      email: Yup.string().required('Email is required').email('Invalid email'),
+      newPassword: Yup.string().required('New Password is required'),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref('newPassword'), null], 'Passwords must match')
+        .required('Confirm Password is required'),
+    }),
+    onSubmit: async (values) => {
+      try {
+        const res = await updateProfile({
+          _id: userInfo._id,
+          oldPassword: values.oldPassword,
+          name: values.name,
+          email: values.email,
+          newPassword: values.newPassword,
+        }).unwrap();
+        dispatch(setCredentials({ ...res }));
+        toast.success('Profile updated!');
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    },
+  });
 
-    useEffect(() => {
-        setName(userInfo.name)
-        setEmail(userInfo.email)
-    }, [userInfo.setName, userInfo.setEmail])
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        if(password !== confirmPassword){
-            toast.error('Passwords do not match')
-        }else{
-            try{
-                const res = await updateProfile({
-                    _id: userInfo._id,
-                    name,
-                    email,
-                    password
-                }).unwrap()
-                dispatch(setCredentials(...res))
-                toast.success('Profile updated!')
-            }catch(err){
-                toast.error(err?.data?.message || err.error)
-            }
-        }
-    }
   return (
-    <>
-        <FormContainer>
-            <h1>Update Profile</h1>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className='my-2' controlId='name'>
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type='text' placeholder='Dodong Skwerut'
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}></Form.Control>
-                </Form.Group>
+    <Container component="main" maxWidth="xs">
+      <Card sx={{ marginTop: 8 }}>
+        <CardHeader title="Update Profile" />
+        <CardContent>
+          <form onSubmit={formik.handleSubmit}>
+           
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="name"
+              label="Name"
+              name="name"
+              autoComplete="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.name && Boolean(formik.errors.name)}
+              helperText={formik.touched.name && formik.errors.name}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+             <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="oldPassword"
+              label="Old Password"
+              name="oldPassword"
+              type={showPassword ? 'text' : 'password'}
+              value={formik.values.oldPassword}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.oldPassword && Boolean(formik.errors.oldPassword)}
+              helperText={formik.touched.oldPassword && formik.errors.oldPassword}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="newPassword"
+              label="New Password"
+              name="newPassword"
+              type={showPassword ? 'text' : 'password'}
+              value={formik.values.newPassword}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.newPassword && Boolean(formik.errors.newPassword)}
+              helperText={formik.touched.newPassword && formik.errors.newPassword}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="confirmPassword"
+              label="Confirm Password"
+              name="confirmPassword"
+              type={showPassword ? 'text' : 'password'}
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+              helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            {isLoading ? (
+              <CircularProgress size={24} />
+            ) : (
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                sx={{ marginTop: 4 }}
+              >
+                Update Profile
+              </Button>
+            )}
+          </form>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
 
-                <Form.Group className='my-2' controlId='email'>
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type='email' placeholder='email@sample.com'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}></Form.Control>
-                </Form.Group>
-
-                <Form.Group className='my-2' controlId='password'>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type='password' placeholder='New Password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}></Form.Control>
-                </Form.Group>
-
-                <Form.Group className='my-2' controlId='confirmPassword'>
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type='password' placeholder='Confirm Password'
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}></Form.Control>
-                </Form.Group>
-
-                {isLoading && <Loader />}
-
-                <Button type='submit' variant='primary' className='mt-3'>
-                    Update Profile
-                </Button>
-            </Form>
-        </FormContainer>
-    </>
-  )
-}
-
-export default ProfilePage
+export default ProfilePage;
