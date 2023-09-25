23830b4097457dc56167e1472c75fbc18b5f0852
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-25 09:54:27 +0800

    DeviceNav.jsx Formatted

--- frontend/src/components/DeviceNav.jsx
+++ frontend/src/components/DeviceNav.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import { Nav } from 'react-bootstrap'
-import { FaSitemap } from 'react-icons/fa'
-import {HiCpuChip} from 'react-icons/hi2'
+import React from "react";
+import { Nav } from "react-bootstrap";
+import { FaSitemap } from "react-icons/fa";
+import { HiCpuChip } from "react-icons/hi2";
 
 const DeviceNav = () => {
   return (
     <>
-        <Nav variant='pills' defaultActiveKey='/devices' className='flex-column'>
-            <Nav.Link className='bg-secondary my-1 py-3 shadow' href='/devices'><span className='h5'><strong><HiCpuChip className='me-3' />All Devices</strong></span></Nav.Link>
-            <Nav.Link className='bg-secondary text-light my-1 py-3 shadow' href='/ip'><span className='h5'><strong><FaSitemap className='me-3' />IP Address</strong></span></Nav.Link>
-        </Nav>
+      <Nav variant="pills" defaultActiveKey="/devices" className="flex-column">
+        <Nav.Link className="bg-secondary my-1 py-3 shadow" href="/devices">
+          <span className="h5">
+            <strong>
+              <HiCpuChip className="me-3" />
+              All Devices
+            </strong>
+          </span>
+        </Nav.Link>
+        <Nav.Link
+          className="bg-secondary text-light my-1 py-3 shadow"
+          href="/ip"
+        >
+          <span className="h5">
+            <strong>
+              <FaSitemap className="me-3" />
+              IP Address
+            </strong>
+          </span>
+        </Nav.Link>
+      </Nav>
     </>
-  )
-}
+  );
+};
 
-export default DeviceNav
+export default DeviceNav;
